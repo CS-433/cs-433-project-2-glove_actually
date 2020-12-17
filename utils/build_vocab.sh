c0f3f05510da91d78869a158bd85cd48333bb3fc
Marie Biolková <36999039+mariegold@users.noreply.github.com>
2020-12-17 17:12:56 +0400

    Delete build_vocab.sh

--- utils/build_vocab.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Note that this script uses GNU-style sed as gsed. On Mac OS, you are required to first https://brew.sh/
-#    brew install gnu-sed
-# on linux, use sed instead of gsed in the command below:
-cat $1 | gsed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > ./data/vocab_full.txt
